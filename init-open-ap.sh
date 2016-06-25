07edd308218598947b94ef1c6e55a684831b75f7
Gergely Imreh <imrehg@gmail.com>
2016-06-25 23:17:12 +0800

    create bridge

--- init-open-ap.sh
+++ init-open-ap.sh
@@ -4,6 +4,15 @@ rfkill block wifi
 
 rfkill unblock wifi
 
+# set up bridge
+ip link add name br0 type bridge
+ip link set br0 up
+ip link set eth0 up
+ip link set eth0 master br0
+
+# Debug: list bridge
+bridge link
+
 ifconfig wlan0 down
 ifconfig wlan0 192.168.42.1
 ifconfig wlan0 up
