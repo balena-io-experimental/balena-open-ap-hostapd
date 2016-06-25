b712f3bf8ec94c9a092e542df9bce65c09df6922
Gergely Imreh <imrehg@gmail.com>
2016-06-25 23:23:09 +0800

    remove bridge, use forwarding actually

--- init-open-ap.sh
+++ init-open-ap.sh
@@ -4,18 +4,15 @@ rfkill block wifi
 
 rfkill unblock wifi
 
-# set up bridge
-ip link add name br0 type bridge
-ip link set br0 up
-ip link set eth0 up
-ip link set eth0 master br0
-
-# Debug: list bridge
-bridge link
-
 ifconfig wlan0 down
 ifconfig wlan0 192.168.42.1
 ifconfig wlan0 up
+
+# Traffic forwarding
+iptables -P FORWARD ACCEPT
+iptables -F FORWARD
+sysctl net.ipv4.ip_forward=1
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 sleep 1
 
